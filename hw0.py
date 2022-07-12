b8361c3688dc500648f3f09cdb6c36b09f06dbd0
Sumaa <79539195+ssayed118@users.noreply.github.com>
2022-07-12 19:23:14 -0400

    Delete hw0.py

--- hw0.py
+++
@@ -1,9 +0,0 @@
-#hw0.py                                                                         
-#Sumaa Sayed                                                                    
-#9/5/19                                                                         
-#Section 16                                                                     
-#ssayed1@umbc.edu                                                               
-#This file is to help students practice                                         
-#using the submission system on the GL                                          
-#servers for CMSC 201.                                                          
-print("This is going to be a great semester!")
